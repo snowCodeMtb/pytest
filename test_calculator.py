bf68f4d2c66e3cdf53543c3db0af33e9e88be199
mcs <itapplefarm@gmail.com>
2024-01-13 13:16:47 -0800

    add comments

--- test_calculator.py
+++ test_calculator.py
@@ -1,17 +1,19 @@
 import pytest
 from calculator import square
 
+# test positive number input 
 def test_positive_num():
     assert square(2) == 4
     assert square(4) == 16
-#test3
+# test negative number input
 def test_negative_num():
     assert square(-2) == 4
     assert square(-4) ==16
-# test2
+# test zero input
 def test_zero():
     assert square(0) == 0
-#test
+
+#test invalid input type
 def test_str():
     with pytest.raises(ValueError):
         square("cat")
